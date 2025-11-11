af60a581373807dc5681b7f3c9ff2b4574ff45ab
Collins Kibet <kollcibe05@gmail.com>
2025-11-11 12:18:26 +0300

    Completed the conditional test

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,73 @@
 
 def admin_login(username, password):
     # your code here
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
-    # your code here
+    if temperature < 40 :
+        return  "It's brisk out there!"
+    elif temperature>=40 and temperature<=65:
+        return "It's a little chilly out there!"
+    elif temperature > 85 :
+        return "It's too dang hot out there!"
+    else: 
+        return "It's perfect out there!"    
     pass
 
 def fizzbuzz(num):
     # your code here
+    if num% 3 == 0 and num%5 == 0 :
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
     # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/": 
+        return num1/num2
+    else:
+        print( "Invalid operation!")
+        return None
     pass
+
+
+
+# #try/except and  finally which is to be computed irregardless
+# def divide(num1, num2):
+#     try: 
+#         quotient = num1/num2 
+#         print(quotient)
+#     except ZeroDivisionError:
+#         print("Error: num2 cannot be a 0") 
+#     except TypeError:
+#         print("Error: nums must be of type int or float ")
+#     finally: 
+#         print("Isn\'t coding fun")
+
+
+# dog = "stupid"
+# dict_map = {
+#     "stupid": "Bosco",
+#     "Clever": "Scooby",
+#     "Ruthless": "Rex",
+#     "Old": "Mzee",
+# }
+
+# name = dict_map.get(dog, "bitch") #get allows us to set a default value "bitch" in this case
+
+   
